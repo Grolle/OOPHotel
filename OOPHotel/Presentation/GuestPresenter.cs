ab070095dc2302ab4da4282cf2dcc03d224772f8
Recall12345 <Noor-sror@hotmail.com>
2026-09-22 13:35:58 +0200

    contact info

--- OOPHotel/Presentation/GuestPresenter.cs
+++ OOPHotel/Presentation/GuestPresenter.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using OOPHotel.Containers;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace OOPHotel.Presentation
 {
     internal class GuestPresenter
     {
+        public void Presenter(Person person)
+        {
+
+            Console.WriteLine("kontaktinformation");
+            Console.WriteLine($"Namn: {person.Name}");//name
+            Console.WriteLine($"Email:{person.Email} ");//email
+            Console.WriteLine($"phone number: {person. Phone}");// phonenumber
+
+
+
+        }
+
+
+
+
+
     }
+    }
+
 }
